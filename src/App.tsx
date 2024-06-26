c865196fe3829fdc89fccbf2168d9c112b47ce42
Janet Vanderpuye <4632924+rhycce@users.noreply.github.com>
2024-06-26 12:03:32 -0400

    Removed serverside rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import DashboardPage from "./pages/DashboardPage";
+import AyodaiLogo from './images/ayodai.png'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path={'/'} element={<DashboardPage/>}/>
-          <Route  path={'/ayodai'} element={<DashboardPage/>}/>
-      </Routes>
-    </BrowserRouter>
+  return (/**
+       <BrowserRouter>
+       <Routes>
+       <Route index path={'/'} element={<DashboardPage/>}/>
+       <Route  path={'/ayodai'} element={<DashboardPage/>}/>
+       </Routes>
+       </BrowserRouter>
+       **/
+      <div className={'container shadow p-3 m-auto bg-white rounded '}>
+          <div className={'row justify-content-md-center'}>
+              <div className={'col-md-auto p-3 bg-white rounded'}>
+                  <img src={AyodaiLogo} alt={'Ayodai logo'}></img>
+              </div>
+          </div>
+          <div className={'row p-3 m-auto bg-white'}>
+              <div className={'col'}>1 Day</div>
+              <div className={'col'}>2 Hours</div>
+              <div className={'col'}>3 Minutes</div>
+              <div className={'col'}>4 Seconds</div>
+          </div>
+          <div className={'row justify-content-md-center'}>
+              <div className={'col-md-auto p-3 bg-white rounded'}>
+                  <label> Under construction</label>
+              </div>
+
+          </div>
+      </div>
   );
 }
 
