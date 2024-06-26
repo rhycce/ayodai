5d4f950a9e0147fea032f1aecf3b3f7e2cc65512
Janet Vanderpuye <4632924+rhycce@users.noreply.github.com>
2024-06-26 11:05:58 -0400

    Added under construction page

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UnderConstructionPage from "./UnderConstructionPage";
+
+function DashboardPage(){
+    return (<><UnderConstructionPage/></>)
+}
+
+export default DashboardPage;
---
+++ src/pages/UnderConstructionPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Image} from "react-bootstrap";
+import AyodaiLogo from '../images/ayodai.png'
+
+function UnderConstructionPage(){
+    return (
+        <div className={'container shadow p-3 m-auto bg-white rounded '}>
+            <div className={'row-cols-3'}>
+                <div className={'p-3 p-3 m-auto bg-white rounded'}>
+                    <Image src={AyodaiLogo}></Image>
+                </div>
+                <div className={'TimerWrapper'}></div>
+            </div>
+            <div className={'row-cols-1'}>
+                <label> Under construction</label>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default UnderConstructionPage;
