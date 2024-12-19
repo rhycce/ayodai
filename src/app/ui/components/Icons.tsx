a7c011dd79247b9889f9b8dad5aab4908029769a
janet <4632924+rhycce@users.noreply.github.com>
2024-12-18 21:22:17 -0500

    Reformatted with prettier #20

--- src/app/ui/components/Icons.tsx
+++ src/app/ui/components/Icons.tsx
@@ -57,7 +57,12 @@ export const Logo = (props: SVGProps<SVGSVGElement>) => (
 )
 
 export const RightArrowHead = (props: SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
     <g
       id="SVGRepo_tracerCarrier"
